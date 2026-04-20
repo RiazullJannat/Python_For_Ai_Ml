ec0fb602f8d740b5e01be153ad58acb7e9074120
RJannat <ramimk097@gmail.com>
2026-04-20 20:11:36 +0600

    complete module 6

---
+++ streamlit.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+st.title("Hello Worlds", anchor=False)
+st.image("https://i.ibb.co.com/KxJvdpz3/t-shirt.jpg")
---
+++ test.py
@@ -0,0 +1,4 @@
+import sys
+print("Current Python Version:", sys.version)
+import numpy as np
+print("NumPy Version:", np.__version__)
